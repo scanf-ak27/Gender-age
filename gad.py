5b1ae98368d84d88112e357f5c1c2c055c3bdcd2
scanf-ak27 <kraayush27@gmail.com>
2023-09-29 23:35:49 +0530

    2nd

--- gad.py
+++ gad.py
@@ -39,6 +39,9 @@ MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
 ageList = ['(0-2)', '(4-6)', '(8-12)', '(15-20)',
            '(25-32)', '(38-43)', '(48-53)', '(60-100)']
 genderList = ['Male', 'Female']
+
+
+
 faceNet = cv2.dnn.readNet(faceModel, faceProto)
 ageNet = cv2.dnn.readNet(ageModel, ageProto)
 genderNet = cv2.dnn.readNet(genderModel, genderProto)
